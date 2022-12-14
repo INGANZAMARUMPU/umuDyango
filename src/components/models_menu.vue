314c267d96184f82846e1b31f2b5e8641a24a9bf
INGANZAMARUMPU <jonkur96@gmail.com>
2022-12-14 15:30:19 +0200

    models menu

--- src/components/models_menu.vue
+++ src/components/models_menu.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="parent">
-    <button>
-      Nouveau model
-    </button>
-    <div class="models">
-      <Model v-for="model in models"/>
-    </div>
-    <button>
-      Telecharger le projet
-    </button>
-  </div>
+	<div class="parent">
+		<h3>Models</h3>
+		<div class="body">
+			<button>
+				[+] Ajouter
+			</button>
+			<div class="models">
+				<Model v-for="model in models"/>
+			</div>
+		</div>
+		<button class="down">
+			Telecharger le projet
+		</button>
+	</div>
 </template>
 
 <script>
@@ -26,5 +29,33 @@ export default {
 }
 </script>
 <style scoped>
-	
+.parent{
+	height: 100vh;
+	max-width: 250px;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+}
+h3{
+	text-align: center;
+	background-color: lightgray;
+	padding: 10px 20px;
+}
+button{
+	width: 100%;
+}
+.body{
+	flex-grow: 1;
+	padding: 10px;
+}
+.down{
+	background-color: steelblue;
+	box-shadow: none;
+}
+.down:hover{
+	background-color: royalblue;
+}
+.down:active{
+	background-color: steelblue;
+}
 </style>
