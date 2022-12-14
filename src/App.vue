169b843b430a622b27446d05088c91296d49deae
INGANZAMARUMPU <jonkur96@gmail.com>
2022-12-14 15:04:08 +0200

    basics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
+  <ModelsMenu/>
+  <Tabs/>
   <router-view/>
 </template>
-
+<script>
+import ModelsMenu from "./components/models_menu"
+import Tabs from "./components/tabs"
+export default {
+  components:{
+    ModelsMenu,
+    Tabs
+  }
+}
+</script>
 <style src="./style.css">
 </style>
---
+++ src/components/models_menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="parent">
+    <button>
+      Nouveau model
+    </button>
+    <div class="models">
+      <Model v-for="model in models"/>
+    </div>
+    <button>
+      Telecharger le projet
+    </button>
+  </div>
+</template>
+
+<script>
+import Model from "./model_item"
+export default {
+  components: {
+    Model
+  },
+  data(){
+    return {
+      models:[]
+    }
+  }
+}
+</script>
+<style scoped>
+	
+</style>
