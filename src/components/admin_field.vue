71689805599a1ad147aac7a9abe6cb0eb7206a84
INGANZAMARUMPU <jonkur96@gmail.com>
2022-12-19 23:09:21 +0200

    create admin page

---
+++ src/components/admin_field.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="item" @dblclick="editName">
+		<input
+			type="text"
+			placeholder="nom du item"
+			@keyup.enter="renameField"
+			v-model="nom"
+			v-if="item.name.length==0 || edit">
+		<h4 v-else>
+			{{ item.name }}
+		</h4>
+		<div class="right">
+			<span class="btn red">delete</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["item"],
+  data(){
+    return {
+      edit: false,
+      nom:"",
+    }
+  },
+  methods:{
+  	renameField(){
+  		this.item.name = ""
+  		this.nom = this.nom.replace("-", " ")
+  		for(let x of this.nom.split(" ")){
+  			this.item.name += x.charAt(0).toUpperCase() + x.slice(1)
+  		}
+  		this.edit = false
+  	},
+  	editName(){
+  		this.nom = this.item.name
+  		this.edit=true
+  	}
+  }
+}
+</script>
+<style scoped>
+.item{
+	padding: 10px;
+	border-bottom: 1px solid lightgray;
+	cursor: default;
+}
+.item:hover{
+	background-color: #f0f0f0;
+}
+ input{
+	padding: 5px;
+	width: 100%;
+}
+.btn{
+	font-size: .8em;
+}
+.btn:hover{
+	text-decoration: underline;
+	cursor: default;
+}
+.red{
+	color: red;
+}
+.right{
+	display: flex;
+	justify-content: flex-end;
+	height: 15px;
+}
+.right>span{
+	display: none;
+}
+.item:hover .right>span{
+	display: block;
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,8 +1,96 @@
 <template>
   <div class="parent">
-    Admin
+    <div class="body">
+      <div class="fields">
+        <Field
+          v-for="field in model.fields"
+          :item="field"
+          @click="current_field=field"
+          @delete="remove(field)"/>
+      </div>
+      <div class="properties" v-if="temp_field">
+        <div class="field">
+          <h3>{{ temp_field.name }}</h3>
+          <select v-model="temp_field.fields.type">
+            <option
+              v-for="field in $store.state.fields"
+              :value="field.name">
+              {{ field.name }}
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Field from "../components/admin_field"
+export default {
+  components:{ Field },
+  data(){
+    return {
+      model: this.$store.state.current_model,
+      temp_field:null,
+      current_field:{}
+    }
+  },
+  watch:{
+    "$store.state.current_model"(new_val){
+      this.model = new_val
+    },
+    current_field(new_val){
+      this.temp_field = JSON.parse(
+        JSON.stringify(new_val)
+      )
+    },
+    "temp_field.fields.type"(new_val){
+      if(new_val != this.current_field.fields.type){
+        this.temp_field.fields = {
+          type: new_val
+        }
+      }
+    }
+  },
+  computed:{
+    current_field_fields(){
+      let fields = this.$store.state.fields
+      for(let field of fields){
+        if(field.name == this.temp_field.fields.type){
+          return field.fields
+        }
+      }
+      return {}
+    }
+  },
+  methods:{
+  },
+}
 </script>
+
+<style scoped>
+.btns>button{
+  margin-right: 10px;
+}
+.body{
+  display: flex;
+  padding-top: 55px;
+}
+.fields{
+  width: 200px;
+  background-color: white;
+  height: calc(100vh - 90px);
+  overflow-y: scroll;
+  margin-right: 20px;
+}
+.field{
+  margin-bottom: 10px;
+}
+.field>*{
+  display: block;
+}
+.field>input, .field>select{
+  padding: 5px 10px;
+  width: 200px;
+}
+</style>
